52f323a5ceee419e5efa52fe40990f771f892c73
Anderson <adersoosilvaa@gmail.com>
2021-07-08 14:09:16 -0300

    Curos de Delphi
    Curso de Delphi

---
+++ Curso Delphi/CursoDelphi.dpr
@@ -0,0 +1,14 @@
+﻿program CursoDelphi;
+
+uses
+  Vcl.Forms,
+  uPrincipal in 'uPrincipal.pas' {FrmPrincipal};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TFrmPrincipal, FrmPrincipal);
+  Application.Run;
+end.
